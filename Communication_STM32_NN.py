de9d60c05c82347fece358a081b4f9ee214ae477
Maure Mathieu <145270591+mauremathieu@users.noreply.github.com>
2025-03-19 18:50:20 +0100

    connection stm32

--- Communication_STM32_NN.py
+++ Communication_STM32_NN.py
@@ -81,8 +81,8 @@ def evaluate_model_on_STM32(iterations, serial_port):
 
 
 if __name__ == '__main__':
-    X_test = np.load("./MNIST_xtest_NN_C2_16_10.npy")
-    Y_test = np.load("./MNIST_ytest_NN_C2_16_10.npy")
+    X_test = np.load("./data/X_test.npy")
+    Y_test = np.load("./data/Y_test.npy")
 
     with serial.Serial(PORT, 115200, timeout=1) as ser:
         print("Synchronising...")
